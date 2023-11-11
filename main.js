90a3f755c339f4542c8fe835d309b656e3c42efa
D@1l0r14 <dailor.d@gmail.com>
2023-11-11 09:58:40 -0300

    Aula 12

--- main.js
+++ main.js
@@ -1,2 +1,5 @@
 console.log("Hello Git!");
-console.log("Hello alteração");
+const img=document.getElementsByTagName('img')
+console.log(img[0])
+const titulo_principal = document.querySelector('tiltulo_principal')
+console.log(titulo_principal)
